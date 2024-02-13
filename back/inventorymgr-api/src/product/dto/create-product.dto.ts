044291e456b4ee5e674ffa01c2e34d5d4f7be7c2
Albus-489 <smartoctopusz@gmail.com>
2024-02-13 14:21:22 +0200

    product fixed dto

--- back/inventorymgr-api/src/product/dto/create-product.dto.ts
+++ back/inventorymgr-api/src/product/dto/create-product.dto.ts
@@ -1,10 +1,24 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
 export class CreateProductDto {
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   photo: string;
+
+  @IsOptional()
   qtt: number;
+
+  @IsOptional()
   group: string;
+
+  @IsOptional()
   minimum: number;
+
+  @IsOptional()
   available: boolean;
-  lastPurchase: Date;
-  
 }
