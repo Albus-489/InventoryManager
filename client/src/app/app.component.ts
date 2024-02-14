4e271151da0a993493b26af03a535f05c929b152
Albus-489 <smartoctopusz@gmail.com>
2024-02-14 10:39:46 +0200

    modules

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidemenuComponent } from "./components/sidemenu/sidemenu.component";
 import { MainPageComponent } from "./components/main-page/main-page.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, SidemenuComponent, MainPageComponent]
+    imports: [CommonModule, RouterOutlet, RouterModule, SidemenuComponent, MainPageComponent]
 })
 export class AppComponent {
 }
