320ad5d0bd920b39820396de89328dc3a846b84f
Albus-489 <smartoctopusz@gmail.com>
2024-02-14 10:40:41 +0200

    menu item

--- client/src/app/components/menu-item/menu-item.component.ts
+++ client/src/app/components/menu-item/menu-item.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AppComponent } from '../../app.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-item',
   standalone: true,
-  imports: [],
+  imports: [AppComponent, RouterModule],
   templateUrl: './menu-item.component.html',
-  styleUrl: './menu-item.component.scss'
+  styleUrl: './menu-item.component.scss',
 })
 export class MenuItemComponent {
+  @Input() text: string = 'Text';
+  @Input() image_path: string = 'assets/menu-items/default.png';
+  @Input() link: string = 'assets/menu-items/default.png'
 
 }
