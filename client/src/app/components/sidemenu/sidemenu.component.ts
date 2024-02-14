07e90cc686093225d547c9be5d1fce7143e4beec
Albus-489 <smartoctopusz@gmail.com>
2024-02-14 10:40:31 +0200

    side menu

--- client/src/app/components/sidemenu/sidemenu.component.ts
+++ client/src/app/components/sidemenu/sidemenu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../logo/logo.component";
+import { MenuItemComponent } from "../menu-item/menu-item.component";
 
 @Component({
     selector: 'app-sidemenu',
     standalone: true,
     templateUrl: './sidemenu.component.html',
     styleUrl: './sidemenu.component.scss',
-    imports: [LogoComponent]
+    imports: [LogoComponent, MenuItemComponent]
 })
 export class SidemenuComponent {
 
