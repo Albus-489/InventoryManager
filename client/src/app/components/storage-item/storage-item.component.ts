e2cc5f8f38a5abd870dc3dd496f9e4b3dc4e92d5
Albus-489 <smartoctopusz@gmail.com>
2024-02-14 10:39:36 +0200

    storage item init

---
+++ client/src/app/components/storage-item/storage-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storage-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './storage-item.component.html',
+  styleUrl: './storage-item.component.scss'
+})
+export class StorageItemComponent {
+
+}
