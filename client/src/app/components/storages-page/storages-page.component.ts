a4f7ee68f59c4683e846626dc5896664536c8cdd
Albus-489 <smartoctopusz@gmail.com>
2024-02-14 10:40:01 +0200

    storages page init

---
+++ client/src/app/components/storages-page/storages-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storages-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './storages-page.component.html',
+  styleUrl: './storages-page.component.scss'
+})
+export class StoragesPageComponent {
+
+}
