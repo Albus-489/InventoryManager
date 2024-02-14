8bd871d54fc66d117e572da7c0ba9e273a7bf639
Albus-489 <smartoctopusz@gmail.com>
2024-02-14 10:38:59 +0200

    route path for about and storages page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StoragesPageComponent } from './components/storages-page/storages-page.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'storages_page', component: StoragesPageComponent },
+  { path: 'about', component: AboutComponent },
+];
